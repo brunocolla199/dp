7cad6a2a12c8724765a97f61d63adb3febb08035
João Vitor Veronese Vieira <joao.vieira@speedsoftware.com.br>
2018-07-09 16:52:21 -0300

    Adição de arquivos úteis de auxílio (Constantes PHP e Métodos genéricos JS)

---
+++ app/Classes/Constants.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Classes;
+
+class Constants {
+
+    public static $ID_TIPO_SETOR_SETOR_NORMAL = 1;
+    
+    public static $ID_TIPO_SETOR_GRUPO_DE_TREINAMENTO = 2;
+    
+    public static $ID_TIPO_SETOR_GRUPO_DE_DIVULGACAO = 3;
+
+}
+
+
+?>
---
+++ public/js/utils-speed.js
@@ -0,0 +1,51 @@
+function getSiglaDocumento(nomeDocumento) {
+    var obj = {
+        'Instrução de Trabalho'   : 'IT',
+        'Procedimentos de Gestão' : 'PG',
+        'Diretrizes de Gestão'    : 'DG'
+
+    };
+
+    return obj[nomeDocumento];
+}
+
+
+function getSiglaSetor(nomeSetor) {
+    var obj = {
+        "Administrativo"            : "ADM",
+        "Armadores"                 : "ARM",
+        "CDI"                       : "CDI",
+        "Compras"                   : "CMP",
+        "Comercial"                 : "COM",
+        "Comunicação"               : "COC",
+        "Controladoria"             : "COT",
+        "Diretoria"                 : "DIR",
+        "Financeiro"                : "FIN",
+        "Jurídico"                  : "JUR",
+        "Manutenção"                : "MAN",
+        "Meio Ambiente"             : "AMB",
+        "Operação"                  : "OPE", 
+        "Pessoas & Organização"     : "P&O",
+        "Processos Aduaneiros"      : "ADU",
+        "Projetos"                  : "PRJ",
+        "Qualidade"                 : "QUA",
+        "Saúde"                     : "SOC",
+        "Segurança do Trabalho"     : "SET",
+        "Segurança Patrimonial"     : "SEP",
+        "Tecnologia da Informação"  : "TEC",
+        "Transporte"                : "TRP"
+    };
+
+    return obj[nomeSetor];
+}
+
+
+function buildDocumentCod(tipoDocumento, setor) {
+    var siglaDoc = getSiglaDocumento(tipoDocumento);
+    var siglaSetor = "";
+
+    if(siglaDoc == "IT") siglaSetor = "-" + getSiglaSetor(setor);
+    
+    var valorFinal = siglaDoc + siglaSetor;
+    return valorFinal;
+}
