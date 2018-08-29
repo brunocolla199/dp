6fcf8dc1393570b3929de6cac1e768ded86e3194
João Vitor Veronese Vieira <joao.vieira@speedsoftware.com.br>
2018-08-29 11:13:03 -0300

    Não entendi porque está aqui esse file, devia ter commitado dois anos atrás hueheu

--- app/Jobs/SendEmailsJob.php
+++ app/Jobs/SendEmailsJob.php
@@ -8,30 +8,57 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Support\Facades\Mail;
-use App\Mail\NecessitaRevisao;
+use App\Mail\TemplateEmail;
 
 class SendEmailsJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    // Propriedades do envio
     protected $destinatarios;
-    protected $documento;
-    protected $responsavelPelaAcao;
-    protected $tipo;
     protected $assunto;
+    
+    // Ícone
+    protected $icon;
+
+    // Frase 1
+    protected $contentF1_P1;
+    protected $codeF1;
+    protected $contentF1_P2;
+
+    // Frase 2
+    protected $labelF2;
+    protected $valueF2;
+
+    // Frase 3
+    protected $labelF3;
+    protected $valueF3;
+    protected $label2_F3;
+    protected $value2_F3;
 
     /**
      * Create a new job instance.
      *
      * @return void
      */
-    public function __construct($destinatarios, $documento, $responsavelPelaAcao, $tipo, $assunto)
+    public function __construct($destinatarios, $assunto, $icon, $contentF1_P1, $codeF1, $contentF1_P2, $labelF2, $valueF2, $labelF3, $valueF3, $label2_F3, $value2_F3)
     {
         $this->destinatarios = $destinatarios;
-        $this->documento = $documento;
-        $this->responsavelPelaAcao = $responsavelPelaAcao;
-        $this->tipo = $tipo;
         $this->assunto = $assunto;
+
+        $this->icon = $icon;
+
+        $this->contentF1_P1 = $contentF1_P1;
+        $this->codeF1 = $codeF1;
+        $this->contentF1_P2 = $contentF1_P2;
+
+        $this->labelF2 = $labelF2;
+        $this->valueF2 = $valueF2;
+
+        $this->labelF3 = $labelF3;
+        $this->valueF3 = $valueF3;
+        $this->label2_F3 = $label2_F3;
+        $this->value2_F3 = $value2_F3;
     }
 
     /**
@@ -41,6 +68,6 @@ class SendEmailsJob implements ShouldQueue
      */
     public function handle(Mail $mail)
     {
-        $mail::to($this->destinatarios)->send(new NecessitaRevisao($this->documento, $this->responsavelPelaAcao, $this->tipo, $this->assunto));
+        $mail::to($this->destinatarios)->send(new TemplateEmail($this->assunto,   $this->icon, $this->contentF1_P1, $this->codeF1, $this->contentF1_P2, $this->labelF2, $this->valueF2, $this->labelF3, $this->valueF3, $this->label2_F3, $this->value2_F3));
     }
 }
