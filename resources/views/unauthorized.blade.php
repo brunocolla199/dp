399c88c1bd2fdf78dd711b616d83a29d71a0410b
João Vitor Veronese Vieira <joao.vieira@speedsoftware.com.br>
2018-11-12 11:23:37 -0200

    #QuickFix - Criando Middleware para telas que somente usuários administradores podem acessar

---
+++ resources/views/unauthorized.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="icon" href="{{ asset('logouri.png') }}">
+        <title>Uri</title>
+	</head>
+
+	<style type="text/css">
+		body{
+		    font-family: 'Montserrat', sans-serif;
+		    margin-top: 30px;
+		    background-color: #1976D2;
+		    background-color: #42A5F5;
+		    text-align: center;
+		    color: #fff;
+		}
+		.error-heading{
+		    margin: 50px auto;
+		    width: 250px;
+		    border: 5px solid #fff;
+		    font-size: 126px;
+		    line-height: 126px;
+		    border-radius: 30px;
+		    text-shadow: 6px 6px 5px #000;
+		}
+		.error-heading img{
+		    width: 100%;
+		}
+		.error-main h1{
+		    font-size: 72px;
+		    margin: 0px;
+		    color: #42A5F5;
+		    text-shadow: 0px 0px 5px #fff;
+		}
+	</style>
+
+	<body>
+		<div class="error-main">
+			<h1>Oops!</h1>
+			<div class="error-heading">403</div>
+			<h2>Você não tem permissão para acessar essa página!</h2>
+		</div>
+	</body>
+</html>
