c4863b025753e15551eede58ff2faa624853994d
Zyad Khalil <zyad@speedsoftware.com.br>
2019-03-12 16:04:12 -0300

    IS#115 - Erro na edição de documetos OnlyOffice (CACHE)³

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,9 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <meta http-equiv='cache-control' content='no-cache'>
-    <meta http-equiv='expires' content='0'>
-    <meta http-equiv='pragma' content='no-cache'>
     
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
