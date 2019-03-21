a4c7a46e097804584f04c4c8511cf8ed34296ab5
Zyad Khalil <zyad@speedsoftware.com.br>
2019-03-21 09:47:38 -0300

    Google Analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,6 +25,16 @@
     <link href="{{ asset('css/colors/blue.css') }}" id="theme" rel="stylesheet">
 
 
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-130449478-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-130449478-1');
+    </script>
+
+
 
     <!-- Others -->
     <!-- Toast CSS -->
