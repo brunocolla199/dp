cf7f342d48cdcb2d369796d434f4754275130cea
joao.vieira <joao.vieira@speedsoftware.com.br>
2019-08-01 15:29:07 -0300

    #SimpleChange - Removendo aquela mensagem do dashboard que está lá desde o ínicio do sistema e fazendo um teste de e-mail

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,6 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
 
                 <div class="panel-body">
                     @if (session('status'))
@@ -13,8 +12,6 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    Aqui serão colocados os gráficos. Os mesmos ainda não estão presentes em virtude de não existirem dados suficientes para alimentá-los.
                 </div>
             </div>
         </div>
